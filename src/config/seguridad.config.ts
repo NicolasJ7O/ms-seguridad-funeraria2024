012d1782f246f4f671c9d6760311022d602a2eb8
Nicolas <nicolasjimenezospina0@gmail.com>
2024-04-22 11:52:57 -0500

    Se actualiza información sensible

--- src/config/seguridad.config.ts
+++ src/config/seguridad.config.ts
@@ -1,12 +1,11 @@
 export namespace ConfiguracionSeguridad {
-  export const claveJWT = process.env.SECRET_PASSWORD_JWT;
+  export const claveJWT = "Nico12345*";
   export const menuUsuarioId = "63d04fcd013402040c040968";
   export const listarAccion = "listar";
   export const guardarAccion = "guardar";
   export const editarAccion = "editar";
   export const eliminarAccion = "eliminar";
   export const descargarAccion = "descargar";
-  //export const mongodbConnectionString = process.env.CONNECTION_STRING_MONGODB;
-  export const mongodbConnectionString = "mongodb+srv://NicolasJO:Nico3210@cluster0.fgyofag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  export const mongodbConnectionString = process.env.CONNECTION_STRING_MONGODB;
   export const rolUsuarioPublico = "6447fb780692cf465038ff62";
 }
